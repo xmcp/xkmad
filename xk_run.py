f0411511948e1b07289b2f3f4d38d2fedf898b94
xmcp <xiaoyuanan2000@sina.com>
2017-01-18 20:15:34 +0800

    v1.3
    add timeout and auto-retry

--- xk_run.py
+++ xk_run.py
@@ -1,26 +1,35 @@
 #coding=utf-8
 import dill
-
+import traceback
 print('=== 加载选课配置')
 with open('save.bin','rb') as f:
     conf=dill.load(f)
 
 s=conf['session']
+TIMEOUT=15
 
 for uri,(csrf_token,choice) in conf['selects'].items():
     print('=== %s'%uri)
     if choice==-666:
-        print('-> 不更改')
+        print(' -> 不更改')
         continue
     else:
-        print('-> 选择 %d'%choice)
-        res=s.post(uri,data=dict(
-            __RequestVerificationToken=csrf_token,
-            rdoId=choice
-        ))
-        res.raise_for_status()
-        if res.json()['Status']!=1:
-            print('选课失败',res.json()['Message'])
-            raise RuntimeError()
-
+        print(' -> 选择 %d'%choice)
+        while True:
+            try:
+                res=s.post(uri,data=dict(
+                    __RequestVerificationToken=csrf_token,
+                    rdoId=choice
+                ),timeout=TIMEOUT)
+                res.raise_for_status()
+                if res.json()['Status']!=1:
+                    raise RuntimeError(res.json()['Message'])
+            except:
+                traceback.print_exc()
+                print(' !! 正在重试')
+            else:
+                print(' ^^ 选课成功')
+                break
+                
 print('=== 完成')
+input()
